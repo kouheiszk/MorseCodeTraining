aa5c0687db9a6360aa6ff01d7d9c773b1f3af2f0
Kouhei Suzuki <kouhei.szk@gmail.com>
2014-01-20 01:18:00 +0900

    Add table view for setting scene

---
+++ MorseCodeTraining/MCTSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  MCTSettingViewController.h
+//  MorseCodeTraining
+//
+//  Created by Suzuki Kouhei on 2014/01/20.
+//  Copyright (c) 2014年 Suzuki Kouhei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCTSettingViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ MorseCodeTraining/MCTSettingViewController.m
@@ -0,0 +1,68 @@
+//
+//  MCTSettingViewController.m
+//  MorseCodeTraining
+//
+//  Created by Suzuki Kouhei on 2014/01/20.
+//  Copyright (c) 2014年 Suzuki Kouhei. All rights reserved.
+//
+
+#import "MCTSettingViewController.h"
+
+@interface MCTSettingViewController ()
+
+@end
+
+@implementation MCTSettingViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    [self.navigationController setTitle:@"設定"];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if(cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%d", indexPath.row];
+
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self performSegueWithIdentifier:@"selectSettingTableCell" sender:self];
+}
+
+#pragma mark - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+@end
